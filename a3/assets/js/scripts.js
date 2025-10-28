78105a9946e4aeba5902f748999becb98bc07b71
Tamana Ali <s4097208@student.rmit.edu.au>
2025-10-29 05:08:16 +1100

    filter skills by category

--- a3/assets/js/scripts.js
+++ a3/assets/js/scripts.js
@@ -2,9 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const modalImage = document.getElementById("modalImage");
   const images = document.querySelectorAll('[data-bs-target="#imageModal"]');
 
+  const filter = document.getElementById("categoryFilter");
+  const items = document.querySelectorAll(".gallery-item");
+
   images.forEach((img) => {
     img.addEventListener("click", function () {
       modalImage.src = this.src;
+    });
+  });
+  filter.addEventListener("change", function () {
+    const selected = this.value;
+
+    items.forEach((item) => {
+      const category = item.getAttribute("data-category");
+      if (selected === "all" || category === selected) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
     });
   });
 
@@ -14,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
   fileInput.addEventListener("change", function () {
     errorMsg.style.display = "none";
 
-    if (!this.value.match(/\.(jpg|jpeg|png|gif|webp|svg)$/i)) {
+    if (!this.value.match(/.(jpg|jpeg|png|gif|webp|svg)$/i)) {
       errorMsg.style.display = "block";
       this.value = "";
     }
